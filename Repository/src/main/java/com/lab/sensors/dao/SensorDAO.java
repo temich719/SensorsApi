5b3f4880dc5926c3f480fdfc8277995e5c7dc7d4
temich719 <temich719@mail.ru>
2022-10-27 20:43:53 +0300

    Data access implementation

--- Repository/src/main/java/com/lab/sensors/dao/SensorDAO.java
+++ Repository/src/main/java/com/lab/sensors/dao/SensorDAO.java
@@ -1,6 +1,7 @@
 package com.lab.sensors.dao;
 
 import com.lab.sensors.entity.Sensor;
+import com.lab.sensors.exception.RepositoryException;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,11 +10,11 @@ public interface SensorDAO {
 
     List<Sensor> getSensorsList(int page, int size);
 
-    List<Sensor> searchSensorsByKeyWord(String keyword);
+    Optional<List<Sensor>> searchSensorsByKeyWord(String keyword, int page, int size);
 
-    void editSensor(long id, Sensor sensor);
+    void editSensor(long id, Sensor sensor) throws RepositoryException;
 
-    void deleteSensor(long id);
+    void deleteSensor(long id) throws RepositoryException;
 
     void addSensor(Sensor sensor);
 
--- Repository/src/main/java/com/lab/sensors/dao/impl/SensorDAOImpl.java
+++ Repository/src/main/java/com/lab/sensors/dao/impl/SensorDAOImpl.java
@@ -2,17 +2,19 @@ package com.lab.sensors.dao.impl;
 
 import com.lab.sensors.dao.SensorDAO;
 import com.lab.sensors.entity.Sensor;
+import com.lab.sensors.exception.RepositoryException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.*;
 import java.util.List;
 import java.util.Optional;
+
+import static com.lab.sensors.stringStorage.RepositoryStringStorage.*;
 
 @Repository
 public class SensorDAOImpl implements SensorDAO {
@@ -38,22 +40,53 @@ public class SensorDAOImpl implements SensorDAO {
     }
 
     @Override
-    public List<Sensor> searchSensorsByKeyWord(String keyword) {
-        Session session = sessionFactory.getCurrentSession();
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<Sensor> criteriaQuery = criteriaBuilder.createQuery(Sensor.class);
-        return null;
+    public Optional<List<Sensor>> searchSensorsByKeyWord(String keyword, int page, int size) {
+        Optional<List<Sensor>> optionalSensors;
+        try {
+            Session session = sessionFactory.getCurrentSession();
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Sensor> criteriaQuery = criteriaBuilder.createQuery(Sensor.class);
+            Root<Sensor> sensorRoot = criteriaQuery.from(Sensor.class);
+            ParameterExpression<String> parameterExpression = criteriaBuilder.parameter(String.class);
+            Predicate likeName = criteriaBuilder.like(sensorRoot.get(NAME), parameterExpression);
+            Predicate likeModel = criteriaBuilder.like(sensorRoot.get(MODEL), parameterExpression);
+            Predicate likeType = criteriaBuilder.like(sensorRoot.get(TYPE), parameterExpression);
+            Predicate likeRange = criteriaBuilder.like(sensorRoot.get(RANGE), parameterExpression);
+            Predicate likeUnit = criteriaBuilder.like(sensorRoot.get(UNIT), parameterExpression);
+            Predicate likeLocation = criteriaBuilder.like(sensorRoot.get(LOCATION), parameterExpression);
+            Predicate likeDescription = criteriaBuilder.like(sensorRoot.get(DESCRIPTION), parameterExpression);
+            Predicate orCondition = criteriaBuilder.or(likeName, likeModel, likeType, likeRange, likeUnit, likeLocation, likeDescription);
+            criteriaQuery.select(sensorRoot).where(orCondition);
+            Query<Sensor> query = session.createQuery(criteriaQuery);
+            query.setParameter(parameterExpression, PERCENT + keyword + PERCENT);
+            List<Sensor> sensors = query.setFirstResult((page - 1) * size).setMaxResults(size).getResultList();
+            optionalSensors = Optional.of(sensors);
+        } catch (NoResultException e) {
+            optionalSensors = Optional.empty();
+        }
+        return optionalSensors;
     }
 
     @Override
-    public void editSensor(long id, Sensor sensor) {
-        sessionFactory.getCurrentSession().update(sensor);
+    public void editSensor(long id, Sensor sensor) throws RepositoryException {
+        Optional<Sensor> optionalSensor = getSensorById(id);
+        if (optionalSensor.isPresent()) {
+            Sensor sessionSensor = optionalSensor.get();
+            sessionFactory.getCurrentSession().detach(sessionSensor);
+            sessionFactory.getCurrentSession().update(sensor);
+        } else {
+            throw new RepositoryException();
+        }
     }
 
     @Override
-    public void deleteSensor(long id) {
+    public void deleteSensor(long id) throws RepositoryException {
         Optional<Sensor> sensor = getSensorById(id);
-        sensor.ifPresent(value -> sessionFactory.getCurrentSession().delete(value));
+        if (sensor.isPresent()) {
+            sessionFactory.getCurrentSession().delete(sensor.get());
+        } else {
+            throw new RepositoryException();
+        }
     }
 
     @Override
