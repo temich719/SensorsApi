c1b8dae848126eb20aec936fbb4cbbecdf4082ff
temich719 <temich719@mail.ru>
2022-10-31 23:00:00 +0300

    cors config fixed

--- Web/src/main/java/com/lab/sensors/Application.java
+++ Web/src/main/java/com/lab/sensors/Application.java
@@ -3,12 +3,28 @@ package com.lab.sensors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 
 }
